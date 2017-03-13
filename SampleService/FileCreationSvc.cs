15f7fcc7fed41990ecb2d08506dd30e995a3a231
Samuel Ezeala <scezeala@unionbankng.com>
2017-03-13 16:24:33 +0100

    first commit

---
+++ SampleService/FileCreationSvc.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleService
+{
+    public partial class FileCreationSvc : ServiceBase
+    {
+        public FileCreationSvc()
+        {
+            InitializeComponent();
+            InitializeScheduler();
+        }
+
+        private void InitializeScheduler()
+        {
+            Scheduler oScheduler = new Scheduler();
+            oScheduler.Start();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
---
+++ SampleService/Scheduler.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace SampleService
+{
+    class Scheduler
+    {
+        System.Timers.Timer oTimer = null;
+        double interval = 20000;
+     
+        public void Start()
+        {
+            oTimer = new System.Timers.Timer(interval);
+            oTimer.AutoReset = true;
+            oTimer.Enabled = true;
+            oTimer.Start();
+
+            oTimer.Elapsed += new System.Timers.ElapsedEventHandler(oTimer_Elapsed);
+
+        }
+
+        private void oTimer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            FIleCreation();
+        }
+
+        void FIleCreation()
+        {
+            string sFilePath = @"C:\Users\scezeala\Documents\SampleService\SampleFile.txt";
+            string sDateTime = DateTime.Now.ToString();
+
+            using (StreamWriter oFileWrite = new StreamWriter(sFilePath, true))
+            {
+                oFileWrite.WriteLine(sDateTime);
+            }
+            
+        }
+    }
+}
